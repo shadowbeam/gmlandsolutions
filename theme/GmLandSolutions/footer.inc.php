de1a4942377de07953e907b5c7ac79aeaf111e2b
shadowbeam <allan.watson91@gmail.com>
2014-07-24 11:45:11 +0100

    footer for site

--- theme/GmLandSolutions/footer.inc.php
+++ theme/GmLandSolutions/footer.inc.php
@@ -1,27 +1,25 @@
 <?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); } 
 /****************************************************
 *
-* @File: 			footer.inc.php
+* @File: 		footer.inc.php
 * @Package:		GetSimple
-* @Action:		Innovation theme for GetSimple CMS
+* @Action:		GMLandSolutions theme for GetSimple CMS
 *
 *****************************************************/
 ?>
 
-	<!-- site footer -->
-	<footer class="clearfix" >
-		
-		<?php get_footer(); ?>
-		
-		<!-- 
-			Theme Credits
-			Please consider keeping the links to the developer and GetSimple if you use this theme
-		-->
-	 	<div class="wrapper">
-			<div class="left"><?php echo date('Y'); ?> <a href="<?php get_site_url(); ?>" ><?php get_site_name(); ?></a></div>
-			<div class="right">Innovation Theme by <a href="http://www.cagintranet.com" >Cagintranet</a> &middot; <?php get_site_credits(); ?></div>
+<!-- site footer -->
+<footer class="clearfix" >
+
+	<?php get_footer(); ?>
+
+	<div class="container">
+		<div class="left"></div>
+		<div class="pull-right">
+			<?php echo date('Y'); ?> <a href="<?php get_site_url(); ?>" ><?php get_site_name(); ?></a><br>
+			Designed by <a href="http://www.allanwatson.me" >Allan Watson</a> &middot; <?php get_site_credits(); ?></div>
 		</div>
 	</footer>
-	 
+
 </body>
 </html>
